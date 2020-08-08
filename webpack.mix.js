aac87d14180e1fc82899775a3b92cad0bb5b8cc5
Terry Upton <terry@amasci.co.uk>
2020-08-08 18:41:47 +0100

    update to move to mix 5.0 and update of all dependencies

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,102 +3,95 @@ const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 require("laravel-mix-purgecss");
-
+require('laravel-mix-versionhash');
 
 // Set the public path to resolve any potential issues with version.
 mix
-    .setPublicPath(path.resolve('./'))
+    .setPublicPath(settings.paths.build.base)
     .sass(settings.paths.src.sass + 'style.scss', settings.paths.build.css)
-
+    
     // JS to Concatenate and Minify JS files using Webpack bundler.
     .js(settings.paths.src.js + 'index.js', settings.paths.build.js)
-
+    
     // Extract splits the JS into Vendor, Manfiest and Index
     .extract()
-    // .extract([
-    //     'lazysizes',
-    //     'picturefill',
-    //     'jquery'
-    // ])
-
-    // Scripts Minifies Scripts - Use for Legacy Projects
-    // .scripts([settings.paths.src.js + 'classie.js'], settings.paths.build.js + 'classie.js')
-
-    // .autoload({
-    //     jQuery: 'jquery',
-    //     $: 'jquery',
-    //     jquery: 'jquery',
-    // })
-
-
+    
     .options({
-        processCssUrls: false,
-        postCss: [
-            tailwindcss(settings.paths.tailwind.config),
-            require('autoprefixer')({
-                grid: "autoplace"
-            }),
-            require('postcss-pxtorem')({
-                propList: ['*'],
-                // propList: ['font', 'font-size', 'line-height', 'letter-spacing', 'width', 'height', 'max-width', 'min-width', 'max-height', 'min-height', 'margin-top', 'margin-left', 'margin-bottom', 'margin-right', 'padding-top', 'padding-left', 'padding-bottom', 'padding-right'],
-            }),
-            require('postcss-sorting')({
-                "properties-order": "alphabetical"
-            })
-        ],
+      processCssUrls: false,
+      postCss: [
+        tailwindcss(settings.paths.tailwind.config),
+        require('autoprefixer')(),
+        require('postcss-pxtorem')({
+          propList: ['*'],
+        }),
+        require('postcss-sorting')({
+          "properties-order": "alphabetical"
+        })
+      ],
     })
-
+    
     .browserSync({
-        proxy: settings.urls.local,
-        port: 8080,
-        files: [
-            settings.paths.tailwind.config,
-            settings.paths.build.css + '{*,**/*}.css',
-            settings.paths.build.js + '{*,**/*}.js',
-            settings.paths.templates.base + '**/*.{html,twig}',
-        ]
+      proxy: settings.urls.local,
+      port: 8080,
+      files: [
+        settings.paths.tailwind.config,
+        settings.paths.build.css + '{*,**/*}.css',
+        settings.paths.build.js + '{*,**/*}.js',
+        settings.paths.templates.base + '**/*.{html,twig}',
+      ]
     })
-
-    .purgeCss({
-        enabled: mix.inProduction(),
-        globs: [
-            path.join(__dirname, settings.paths.templates.base + '**/*.{html,twig}'),
-            path.join(__dirname, settings.paths.build.js + '**/*.js'),
-            path.join(__dirname, settings.paths.src.js + '**/*.js'),
-        ],
-        extensions: ["html", "js", "php", "vue", "twig"],
-        whitelistPatterns: [/ls-blur-up-img/, /^mfp-/],
-        // whitelistPatternsChildren: [/body/, /ls-blur-up-img/],
-    })
-
+    
     //IMAGE and SVG MIN
     .webpackConfig({
-        plugins: [
-            new CopyWebpackPlugin([
-                {from: settings.paths.src.svg, to: settings.paths.build.svg},
-                {from: settings.paths.src.img, to: settings.paths.build.img}
-                // {from: 'src/img', to: 'web/assets/img'}
-            ]),
-            new ImageminPlugin({
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                svgo: {
-                    plugins: [
-                        {removeViewBox: false},
-                        {removeDimensions: true},
-                    ]
-                },
-                plugins:
-                    [
-                        imageminMozjpeg({
-                            quality: 80,
-                        })
-                    ]
-            })
-        ]
+      plugins: [
+        new CopyWebpackPlugin({
+          patterns: [
+            {from: settings.paths.src.svg, to: settings.paths.build.svg},
+            {from: settings.paths.src.img, to: settings.paths.build.img}
+          ],
+        }),
+        new ImageminPlugin({
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          svgo: {
+            plugins: [
+              {removeViewBox: false},
+              {removeDimensions: true},
+            ]
+          },
+          plugins:
+              [
+                imageminMozjpeg({
+                  quality: 80,
+                })
+              ]
+        })
+      ]
     });
 
 if (mix.inProduction()) {
-    mix.version();
+  mix.versionHash();
+  
+  mix.purgeCss({
+    content: [
+      settings.paths.templates.base + "**/*.{html,twig}",
+      settings.paths.src.js + "**/*.{js,vue}",
+    ],
+    // Include any special characters you're using in this regular expression
+    defaultExtractor: content => content.match(/[\w-/:%]+(?<!:)/g) || [],
+    whitelistPatterns: [],
+    folders: ['src'],
+  });
+  
+  //CLEAN HASHED VERSIONS
+  mix.webpackConfig({
+    plugins: [
+      new CleanWebpackPlugin({
+        cleanOnceBeforeBuildPatterns: settings.paths.build.clean,
+        dry: false,
+      })
+    ]
+  });
 }
