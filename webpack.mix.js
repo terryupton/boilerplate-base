da75a95a38a6acffaf46da2f40736cbcb504a81a
Terry Upton <terry@amasci.co.uk>
2021-02-07 11:46:27 +0000

    Updating laravel mix config and settings
    Add optional (commented out) code in the mx file for:
    - Image and SVG Minification
    - Critical CSS
    - Version Hash
    - Clean before build
    
    - Now uses postcss config file instead of bei g defined inside the mix file.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,97 +1,96 @@
-const settings = require("./settings.json");
-const mix = require('laravel-mix');
-const tailwindcss = require('tailwindcss');
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-require("laravel-mix-purgecss");
-require('laravel-mix-versionhash');
+let settings = require("./settings.json");
+let mix = require('laravel-mix');
+
+//DO we need image and SVG minifcation?
+// const ImageminPlugin = require('imagemin-webpack-plugin').default;
+// const CopyWebpackPlugin = require('copy-webpack-plugin');
+// const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+// const imageminMozjpeg = require('imagemin-mozjpeg');
+
+// Do we need version control?
+// require('laravel-mix-versionhash');
+
+// Do we need Critical CSS?
+// require('laravel-mix-criticalcss');
+
 
 // Set the public path to resolve any potential issues with version.
-mix
-    .setPublicPath(settings.paths.build.base)
-    .sass(settings.paths.src.sass + 'style.scss', settings.paths.build.css)
-    
-    // JS to Concatenate and Minify JS files using Webpack bundler.
-    .js(settings.paths.src.js + 'index.js', settings.paths.build.js)
-    
-    // Extract splits the JS into Vendor, Manfiest and Index
-    .extract()
-    
-    .options({
-      processCssUrls: false,
-      postCss: [
-        tailwindcss(settings.paths.tailwind.config),
-        require('autoprefixer')(),
-        require('postcss-pxtorem')({
-          propList: ['*'],
-        }),
-        require('postcss-sorting')({
-          "properties-order": "alphabetical"
-        })
-      ],
-    })
-    
-    .browserSync({
-      proxy: settings.urls.local,
-      port: 8080,
-      files: [
-        settings.paths.tailwind.config,
-        settings.paths.build.css + '{*,**/*}.css',
-        settings.paths.build.js + '{*,**/*}.js',
-        settings.paths.templates.base + '**/*.{html,twig}',
-      ]
-    })
-    
-    //IMAGE and SVG MIN
-    .webpackConfig({
-      plugins: [
-        new CopyWebpackPlugin({
-          patterns: [
-            {from: settings.paths.src.svg, to: settings.paths.build.svg},
-            {from: settings.paths.src.img, to: settings.paths.build.img}
-          ],
-        }),
-        new ImageminPlugin({
-          test: /\.(jpe?g|png|gif|svg)$/i,
-          svgo: {
-            plugins: [
-              {removeViewBox: false},
-              {removeDimensions: true},
-            ]
-          },
-          plugins:
-              [
-                imageminMozjpeg({
-                  quality: 80,
-                })
-              ]
-        })
-      ]
-    });
+mix.setPublicPath(settings.paths.build.base);
+mix.postCss(settings.paths.src.css + 'style.pcss', settings.paths.build.css);
 
-if (mix.inProduction()) {
-  mix.versionHash();
-  
-  mix.purgeCss({
-    content: [
-      settings.paths.templates.base + "**/*.{html,twig}",
-      settings.paths.src.js + "**/*.{js,vue}",
-    ],
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(/[\w-/:%]+(?<!:)/g) || [],
-    whitelistPatterns: [],
-    folders: ['src'],
-  });
-  
-  //CLEAN HASHED VERSIONS
-  mix.webpackConfig({
-    plugins: [
-      new CleanWebpackPlugin({
-        cleanOnceBeforeBuildPatterns: settings.paths.build.clean,
-        dry: false,
-      })
-    ]
-  });
-}
+mix.js(settings.paths.src.js + 'index.js', settings.paths.build.js);
+// mix.extract();
+
+mix.browserSync({
+  proxy: settings.urls.local,
+  port: 8080,
+  files: [
+    settings.paths.tailwind.config,
+    settings.paths.build.css + '{*,**/*}.css',
+    settings.paths.build.js + '{*,**/*}.js',
+    settings.paths.templates.base + '**/*.{html,php}',
+  ]
+});
+
+//IMAGE and SVG MIN
+// mix.webpackConfig({
+//   plugins: [
+//     new CopyWebpackPlugin({
+//       patterns: [
+//         {from: settings.paths.src.svg, to: settings.paths.build.svg},
+//         {from: settings.paths.src.img, to: settings.paths.build.img}
+//       ],
+//     }),
+//     new ImageminPlugin({
+//       test: /\.(jpe?g|png|gif|svg)$/i,
+//       svgo: {
+//         plugins: [
+//           {removeViewBox: false},
+//           {removeDimensions: true},
+//         ]
+//       },
+//       plugins:
+//           [
+//             imageminMozjpeg({
+//               quality: 80,
+//             })
+//           ]
+//     })
+//   ]
+// });
+
+// CriticalCSS
+// mix.criticalCss({
+//   enabled: mix.inProduction(),
+//   paths: {
+//     base: settings.urls.criticalcss,
+//     templates: settings.paths.templates.criticalcss
+//   },
+//   urls: [
+//     {url: '', template: 'pages/home'},
+//     {url: '/about', template: 'pages/about'},
+//   ],
+//   options: {
+//     width: 1400,
+//     height: 1200,
+//     minify: true,
+//     user: 'amasci',
+//     pass: 'draft'
+//   },
+// });
+
+
+// PRODUCTION BUILD
+// if (mix.inProduction()) {
+//   mix.versionHash();
+
+//   // Clean Assets before rebuilding in production.
+//   mix.webpackConfig({
+//     plugins: [
+//       new CleanWebpackPlugin({
+//         cleanOnceBeforeBuildPatterns: settings.paths.build.clean,
+//         dry: false,
+//       })
+//     ]
+//   });
+// }
